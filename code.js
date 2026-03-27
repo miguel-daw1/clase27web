3193df68053a14a1d9a3e246c05d63ecb9f56744
Miguel Garcia <agcmiguel15@gmail.com>
2026-03-27 15:17:59 -0600

    empanada

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
 const texto = document.getElementById("titulo")
 const sonido = document.getElementById("sonido")
 const boton = document.getElementById("btn")
+/*cada que presione el boton
+se reproduzca el sonido */
+boton.addEventListener("click",()=>{
+ sonido.play()
+})
